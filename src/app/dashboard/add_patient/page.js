4cadec840d62c35fc2e1755bba78a665e4359aaa
joshi <joseph.ilagan.99.com>
2023-06-26 19:24:18 +0800

    consultation page+buttons+spacing

--- src/app/dashboard/add_patient/page.js
+++ src/app/dashboard/add_patient/page.js
@@ -7,12 +7,12 @@ const AddPatient = () => {
     <div className='mx-auto'>
     <div className='flex'>
       <p>Patient is non-existent on our clinic database</p>
-      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Add to Database</button></Link>
+      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50  font-medium rounded-2xl text-sm px-5 py-2.5 text-center text-xl mr-2 mb-2">Add to Database</button></Link>
      
     </div>
     <div className='flex'>
       <p> No admitted patient at the moment...</p>
-      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Admit a Patient</button></Link>
+      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-2xl px-5 py-2.5 text-center text-xl mr-2 mb-2">Start Admission</button></Link>
     </div>
 
   <div>
@@ -25,42 +25,42 @@ const AddPatient = () => {
               <div className='flex justify-between'>
               <div id="leftsyd">
                 <div>
-                    <label for="idnumber" class="block mb-2 text-sm font-medium text-gray-900 ">Your ID Number</label>
-                    <input type="text" name="idnumber" id="idnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="ID Number" required=""/>
+                    <label for="idnumber" class="block mb-2 font-medium text-gray-900 ">Your ID Number</label>
+                    <input type="text" name="idnumber" id="idnumber" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="ID Number" required=""/>
                 </div>
                 <div>
-                    <label for="firstName" class="block mb-2 text-sm font-medium text-gray-900 ">Your First Name</label>
-                    <input type="text" name="firstName" id="firstName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="First Name" required=""/>
+                    <label for="firstName" class="block mb-2 mt-3 font-medium text-gray-900 ">Your First Name</label>
+                    <input type="text" name="firstName" id="firstName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="First Name" required=""/>
                 </div>
                 <div>
-                    <label for="lastName" class="block mb-2 text-sm font-medium text-gray-900 ">Your Last Name</label>
+                    <label for="lastName" class="block mb-2 mt-3 font-medium text-gray-900 ">Your Last Name</label>
                     <input type="text" name="lastName" id="lastName" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="Last Name" required=""/>
                 </div>
               </div>
               <div id="rightsyd">
                 <div>
-                    <label for="department" class="block mb-2 text-sm font-medium text-gray-900 ">Your department</label>
+                    <label for="department" class="block mb-2 font-medium text-gray-900 ">Your department</label>
                     <input type="text" name="department" id="department" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="department" required=""/>
                 </div>
                 <div>
-                    <label for="course" class="block mb-2 text-sm font-medium text-gray-900 ">Your Course / Position</label>
+                    <label for="course" class="block mb-2 mt-3 font-medium text-gray-900 ">Your Course / Position</label>
                     <input type="text" name="course" id="course" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="Course / Position " required=""/>
                 </div>
                 <div>
-                    <label for="yearlevel" class="block mb-2 text-sm font-medium text-gray-900 ">Your year level</label>
-                    <input type="text" name="yearlevel" id="yearlevel" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5      " placeholder="Year Level" required=""/>
+                    <label for="yearlevel" class="block mb-2 mt-3 font-medium text-gray-900 ">Your year level</label>
+                    <input type="text" name="yearlevel" id="yearlevel" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Year Level" required=""/>
                 </div>
               </div>
               </div>
                 <div>
-                    <label for="symptom" class="block mb-2 text-sm font-medium text-gray-900 ">Sypmtom</label>
+                    <label for="symptom" class="block mb-2 font-medium text-gray-900 ">Sypmtom</label>
                     <textarea name="message" id="symptom" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5" placeholder="Symptom" required=""/>
                 </div>                
                <br/>
 
               <div className='flex justify-between'>
-               <Link href="#"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Cancel</button></Link>
-               <Link href="/dashboard/patient_queueing"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Admit Patient</button></Link>
+               <Link href="/dashboard"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-2xl text-xl px-5 py-2.5 text-center mr-2 mb-2">Cancel</button></Link>
+               <Link href="/dashboard/patient_queueing"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-2xl text-xl px-5 py-2.5  text-center mr-2 mb-2">Admit Patient</button></Link>
                </div>
             </form>
         </div>
--- src/app/dashboard/patient_queueing/page.js
+++ src/app/dashboard/patient_queueing/page.js
@@ -14,13 +14,13 @@ const PatientQueueing = () => {
               <p>Time: 11:10 PM</p>
             </div>
           </div>
-          <div className='flex justify-between items-end pcard'>  
+          <div className='flex justify-between items-end pcard text-lg'>  
             <div>
               <p><strong> Name :</strong> Ichigo Kurasaki</p>
               <p><strong>Course :</strong> Bachelor of Science in Information Technology</p>
               <p><strong>Symptom :</strong> Bruises / Skin Cut</p>
             </div>
-            <Link href="/dashboard/patient_queueing"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Start Consultation</button></Link>
+            <Link href="/dashboard/consultation"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-2xl text-xl px-5 py-2.5 text-center mr-2 mb-2">Start Consultation</button></Link>
           </div>
         </div>
       
@@ -33,13 +33,13 @@ const PatientQueueing = () => {
               <p>Time: 11:15 PM</p>
             </div>
           </div>
-          <div className='flex justify-between items-end pcard'>  
+          <div className='flex justify-between items-end pcard text-lg'>  
             <div>
               <p><strong> Name :</strong> Zangetsu</p>
               <p><strong>Course :</strong> Bachelor of Science in Information Technology</p>
               <p><strong>Symptom :</strong> Burns</p>
             </div>
-            <Link href="/dashboard/patient_queueing"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2">Start Consultation</button></Link>
+            <Link href="/dashboard/consultation"> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300  shadow-lg shadow-green-500/50  font-medium rounded-2xl text-xl px-5 py-2.5 text-center mr-2 mb-2">Start Consultation</button></Link>
           </div>
         </div>
     </div>
