a7a944e184dddd7d6510b0ebffafb9ffe805d5c3
joshi <joseph.ilagan.99.com>
2023-06-26 19:26:35 +0800

    consultation page+buttons+spacing2

--- src/app/dashboard/add_patient/page.js
+++ src/app/dashboard/add_patient/page.js
@@ -7,7 +7,7 @@ const AddPatient = () => {
     <div className='mx-auto'>
     <div className='flex'>
       <p>Patient is non-existent on our clinic database</p>
-      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50  font-medium rounded-2xl text-sm px-5 py-2.5 text-center text-xl mr-2 mb-2">Add to Database</button></Link>
+      <Link href="#" className='pl-20'> <button type="button" class="text-white bg-gradient-to-r from-green-400 via-green-500 to-green-600 hover:bg-gradient-to-br focus:ring-4 focus:outline-none focus:ring-green-300 shadow-lg shadow-green-500/50  font-medium rounded-2xl px-5 py-2.5 text-center text-xl mr-2 mb-2">Add to Database</button></Link>
      
     </div>
     <div className='flex'>
