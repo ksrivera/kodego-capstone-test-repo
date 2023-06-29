f62ccc71639fb88cd5ad7beb960dc564869a1670
joshi <joseph.ilagan.99.com>
2023-06-29 13:18:06 +0800

    addmission form

--- src/app/dashboard/add_patient/page.js
+++ src/app/dashboard/add_patient/page.js
@@ -12,7 +12,7 @@ const AddPatient = () => {
   <div class="patient-queue bg-white rounded-lg shadow">
         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 class="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl ">
-                Register now or see the Principal
+                Admission Form
             </h1>
             <form class="space-y-6 md:space-y-6" action="#">
               <div className='flex justify-between'>
